023eaeacc78793663e825fa6ae6da100398b14b5
Tilak Shenoy <tilaknshenoy@gmail.com>
2020-06-26 20:35:00 +0530

    MemeEntryActivity and MoreActivity added

--- app/src/main/java/com/fx/folx/MemeEntryActivity.java
+++ app/src/main/java/com/fx/folx/MemeEntryActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class PictureEntryActivity extends AppCompatActivity {
+public class MemeEntryActivity extends AppCompatActivity {
 
     private ImageView imageView;
 
@@ -17,12 +17,12 @@ public class PictureEntryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture_entry);
 
-        Button imgContinue = findViewById(R.id.imgContinue);
+        Button imgContinue = findViewById(R.id.memeContinue);
 
         imgContinue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(PictureEntryActivity.this, MemeEntryActivity.class));
+                startActivity(new Intent(MemeEntryActivity.this, SwipeActivity.class));
             }
         });
     }
--- app/src/main/java/com/fx/folx/ProfileActivity.java
+++ app/src/main/java/com/fx/folx/ProfileActivity.java
@@ -2,13 +2,29 @@ package com.fx.folx;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private TextView moreText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        moreText = findViewById(R.id.profileMore);
+
+        moreText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProfileActivity.this, MoreActivity.class));
+            }
+        });
+
     }
 }
